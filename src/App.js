c0e7404c6fbd41f3a6fbdc8cadf517160e3777d2
Valery Nazemkin <vnazemkin@fil-it.ru>
2021-12-25 13:53:44 +0300

    Подключил redux без middleware

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import './App.css'
 import Catalog from './pages/Catalog/Catalog'
+import { loadDataStarted } from './store/app/action'
 
-const App = () => (
-  <div className="App">
-    <Catalog />
-  </div>
-)
+const App = ({ loadData }) => {
+  useEffect(() => {
+    console.log('loadData')
+    loadData()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div className="App">
+      <Catalog />
+    </div>
+  )
+}
 
-export default App
+const mapDispatchToProps = (dispatch) => ({
+  loadData: () => dispatch(loadDataStarted()),
+})
+
+export default connect(null, mapDispatchToProps)(App)
+
--- src/index.js
+++ src/index.js
@@ -7,7 +7,26 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import reducer from './store/app/reducer'
 
-const store = createStore(reducer)
+// const composeEnhancers =
+//   typeof window === 'object' &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+//     }) : compose;
+
+// const enhancer = composeEnhancers(
+//   applyMiddleware(...middleware),
+//   // other store enhancers if any
+// )
+
+//const store = createStore(reducer)//, enhancer);
+
+const store = createStore(
+  reducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+// const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
